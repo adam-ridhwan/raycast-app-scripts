0e349e360fc76f2e43d151e7e907e1c9ac738363
Adam Ridhwan <adamridhwan.1@gmail.com>
2025-02-04 08:57:50 -0500

    update

---
+++ ghostty.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Ghostty
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 👻
+
+# Documentation:
+# @raycast.description Opens Ghostty terminal
+# @raycast.author adam_ridhwan
+# @raycast.authorURL https://raycast.com/adam_ridhwan
+
+open -a "Ghostty"
+#osascript -e 'tell application "System Events" to key code 41 using {command down, option down, control down}'
+
+
