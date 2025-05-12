4057703b30b8a162978c6f0e3c829ce5d1f2e070
Adam Ridhwan <adamridhwan.1@gmail.com>
2025-05-11 20:15:03 -0400

    update

---
+++ sublime.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Sublime
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.description Opens Sublime Text
+# @raycast.author adam_ridhwan
+# @raycast.authorURL https://raycast.com/adam_ridhwan
+
+open -a 'Sublime Text'
+
